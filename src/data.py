910bd04235db900d88e10e28ed38f43a0e8cd3b5
Tinonturja <tinonturja@gmail.com>
2026-08-26 15:58:16 +0600

    Checking the value of inverse n

--- src/data.py
+++ src/data.py
@@ -3,6 +3,14 @@ import pandas as pd
 import os
 
 class Data:
+    """
+    Get any excel file regarding catkin dataset
+    Convert them into dataframe
+    
+    """
     def __init__(self, data_path):
         if os.path.exists(data_path):
             self.data = pd.read_excel(data_path)
+
+        else:
+            raise ValueError("Data path is not right")
