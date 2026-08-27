7af507b6c5fb182629cf4e4584edace04eb4b7e5
Tinonturja <tinonturja@gmail.com>
2026-08-27 21:15:38 +0600

    Dataset has been formed

--- src/data.py
+++ src/data.py
@@ -10,7 +10,21 @@ class Data:
     """
     def __init__(self, data_path):
         if os.path.exists(data_path):
-            self.data = pd.read_excel(data_path)
-
+            self.data = pd.read_excel(data_path, sheet_name = None)
         else:
             raise ValueError("Data path is not right")
+
+    def get_dataframe(self):
+        self.dataframes_dict = {}
+        for sheet, df in self.data.items():
+            if 'catkin' in sheet:
+                self.dataframes_dict[sheet] = df
+        self.sheet_name = [sheet_name for sheet_name in self.dataframes_dict.keys()]
+        return self.dataframes_dict
+
+    def print_df(self):
+        # get the name of the sheet
+        print(f"Dataframe Name: {[sheet_name for sheet_name in self.dataframes_dict.keys()]}")
+        print(f"Isotherm Data Shape: {self.dataframes_dict[self.sheet_name[0]].shape}")
+        print(f"Kinetics Data Shape: {self.dataframes_dict[self.sheet_name[1]].shape}")
+    
